2e1da31b988b2fb7298cb129df8bdb6dba981f8a
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-10-02 14:39:57 -0300

    Add files via upload

---
+++ valormaiorouigualacemlista2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+int A,B,C,D;
+printf("Digite o valor de A: \n");
+scanf("%d",&A);
+printf("Digite o valor de B: \n");
+scanf("%d",&B);
+printf("Digite o valor de C: \n");
+scanf("%d",&C);
+D=A+B+C;
+if (D>=100){
+    printf("O resultado de D e: %d \n",D);
+}
+}    
