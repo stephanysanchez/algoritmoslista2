5f98f84e9e9b97e372b6775c5f80411af7071e62
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-10-02 14:46:25 -0300

    Add files via upload

---
+++ numeropnpag143B.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+float N;
+printf("Digite um numero: \n");
+scanf("%f",&N);
+if (N<0){
+printf("Numero negativo. \n");
+}
+if (N>0){
+printf("Numero positivo. \n");
+}
+}
