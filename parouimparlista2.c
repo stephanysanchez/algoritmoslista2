114ee4be615eb9490c7a0c6a81a740aa08ae8ad5
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-10-02 14:37:32 -0300

    Add files via upload

---
+++ parouimparlista2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+int Numero;
+printf("Informe um valor: \n");
+scanf("%d",&Numero);
+if (Numero %2 ==0)
+    printf("O numero e par. \n");
+else 
+    printf("O numero e impar. \n");  
+}    
